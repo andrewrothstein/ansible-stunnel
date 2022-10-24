2ea3b10946e28bb2010e1be593e220f816cac860
Andrew Rothstein <andrew.rothstein@gmail.com>
2022-10-23 20:01:23 -0400

    stunnel:5.66

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -6,7 +6,7 @@ dl() {
     local -r ver=$1
     local -r url=$MIRROR/stunnel-${ver}.tar.gz.sha256
     printf "  # %s\n" $url
-    printf "  '%s': sha256:%s\n" $ver $(curl -sSL $url | awk '{print $1}')
+    printf "  '%s': sha256:%s\n" $ver $(curl -sSLf $url | awk '{print $1}')
 }
 
-dl ${1:-5.64}
+dl ${1:-5.66}
